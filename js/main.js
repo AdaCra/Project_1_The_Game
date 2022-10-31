a5847e326518be0af2c8f5823222cd9c92b4c184
Adam Hannath <ahannath@gmail.com>
2022-10-31 23:32:57 +0100

    try again

--- js/main.js
+++ js/main.js
@@ -1,36 +1,67 @@
-// <-*-*-*-*-instructions for final loading interaction-*-*-*-*->
+let backgroundEasy,
+	backgroundE2M,
+	backgroundMedium,
+	backgroundM2H,
+	backgroundHard
 
-// <-*-*-*-*-Load game assets, referenced in game.js-*-*-*-*-*>
-const game = new Game()
+let playerType1,
+	playerType2
 
+let fuel,
+	time
+
+let blueTruck,
+	greenVan,
+	redCar,
+	redTruck,
+	yellowCar
+
+let manhole,
+	oilSpill,
+	roadWorks
+
+let bkgndSize = [600, 900]
+let background 
+
+// Load game assets
 function preload() {
-	game.preload()
+	backgroundEasy 		= "./assets/background/roadLanesX4.png";
+	backgroundE2M 		= "./assets/background/roadLanes4To3.png";
+	backgroundMedium 	= "./assets/background/roadLanesX3.png";
+	backgroundM2H 		= "./assets/background/roadLanes3To2.png";
+	backgroundHard 		= "./assets/background/roadLanesX2.png";
+	
+	playerType1 	= "./assets/player/carType1.png";
+	playerType2 	= "./assets/player/carType2.png";
+
+	fuel	= "./assets/collectables/fuel.png";
+	time	= "./assets/collectables/time.png";
+
+	blueTruck	= "./assets/traffic/blueTruck.png";
+	greenVan	= "./assets/traffic/greenVan.png";
+	redCar		= "./assets/traffic/redCar.png";
+	redTruck	= "./assets/traffic/redTruck.png";
+	yellowCar	= "./assets/traffic/yellowCar.png";
+
+	manhole		= "./assets/obstacles/manhole.png";
+	oilSpill	= "./assets/obstacles/oilSpill.png";
+	roadWorks	= "./assets/obstacles/roadWorks.png";
 }
 
 function setup() {
-	createCanvas(600, 800)
+	bkgndSize = [windowHeight/3*2, windowHeight]
+	createCanvas(bkgndSize[0], bkgndSize[1])
+	frameRate(25)
+
 }
 
 function draw() {
-	game.draw()
+	image(backgroundEasy, 0,0);
 }
 
-
-// <-*-*-*-*-movements referenced in player.js file-*-*-*-*->
-function keyPressed() {
-	if (keyCode === 38 || keyCode === 87) {
-		game.player.accelerate()
-	}
-
-    if (keyCode === 37 || keyCode === 65) {
-		game.player.left()
-	}
-
-    if (keyCode === 39 || keyCode === 68) {
-		game.player.right()
-	}
-
-    if (keyCode === 40 || keyCode === 83) {
-		game.player.brake()
-	}
-}
+// function keyPressed() {
+	// if (keyCode === 32) {
+	// 	game.player.jump()
+// 	}
+// }
+ 
